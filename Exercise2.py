bd4ee51004dd02f03424cd9278589384f57872b1
Tyler-Annis <123253725+Tyler-Annis@users.noreply.github.com>
2023-01-21 13:04:06 -0500

    Add files via upload

---
+++ Exercise2.py
@@ -0,0 +1,17 @@
+print('Tyler Annis 1/20/23')
+print('Question')
+print('at what size of a project should you start using version managment software?')
+print('Answer')
+print('you should use your version managment after the first line of code is made')
+print('Question')
+print('Why?')
+print ('Answer')
+print('it allows you to keep track of your work and ajustments')
+echo "# Exercise2.0" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Tyler-Annis/Exercise2.0.git
+git push -u origin main
+
